e7de8bc9edb5c605c82646b25329ee368fadfc62
Dharmendra Rathod <39919825+rathoddharmendra@users.noreply.github.com>
2026-03-14 10:46:38 +0100

    Updated Project Files

--- scripts/loops.sh
+++ scripts/loops.sh
@@ -6,6 +6,18 @@
 # Description: Playing with loops, and various ways to use them
 
 # Define Variables
+cont=0
+    for i in 1 2 3 4 5
+    # for i in eat sleep relax workout
+    # for i in {1..5}
+    do
+        # $cont+=$i
+        # echo $i and $((cont+10))
+        echo >> null
+    done
 
-
-
+    for username in `awk -F: '{print $1}' /etc/passwd`
+    do
+        echo $((cont++)):$username
+        echo $((cont++)):$username >> users_list.txt
+    done
