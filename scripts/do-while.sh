34b7eac96cb9ef414a8b4bf01cb8c738bd469098
Dharmendra Rathod <39919825+rathoddharmendra@users.noreply.github.com>
2026-03-14 11:14:13 +0100

    Updated Project Files

--- scripts/do-while.sh
+++ scripts/do-while.sh
@@ -6,10 +6,27 @@
 # Description: Playing with loops, and various ways to use them
 
 # Define Variables
+uptime | awk -F: '{print $2}'
 
-
-while [ "uptime | awk -F: '{print $2}'" == 52 ]
+while [ "$(uptime | awk -F: '{print $2}')" == 58 ]
 do 
     echo "It's not time yet"
     sleep 2
+done
+
+count=0
+while [ $count -lt 5 ]
+do
+    echo ---$?
+    count=`expr $count + 1`
+    # (($c++ ))
+done
+
+count=10
+while [ $count -gt 0 ]
+do
+    echo countdown 🙏🏻 $count
+    count=`expr $count - 1` # I like this expression
+    # $(( $count - 1 ))
+    sleep 1
 done
